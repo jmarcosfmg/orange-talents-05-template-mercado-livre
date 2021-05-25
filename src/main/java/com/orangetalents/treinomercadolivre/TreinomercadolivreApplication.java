dac65c8bbad5216f50b2f5ebf5c82c097d64ce35
jmarcosfmg <jmarcosfmg@gmail.com>
2021-05-25 10:25:09 -0300

    inicialização do projeto

---
+++ src/main/java/com/orangetalents/treinomercadolivre/TreinomercadolivreApplication.java
@@ -0,0 +1,13 @@
+package com.orangetalents.treinomercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinomercadolivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinomercadolivreApplication.class, args);
+	}
+
+}
